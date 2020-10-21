cfcb750f83659efda494d3998adc128536a5be3d
systemFood <cukjsystem@gmail.com>
2020-10-21 15:38:33 +0700

    update ui from 21-10-63

--- lib/screens/show_shop_food_menu.dart
+++ lib/screens/show_shop_food_menu.dart
@@ -1,27 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:systemfood/model/user_model.dart';
+import 'package:systemfood/utility/my_style.dart';
+
+import 'package:systemfood/widget/about_shop.dart';
+import 'package:systemfood/widget/show_menu_food.dart';
 
 class ShowShopFoodMenu extends StatefulWidget {
   final UserModel userModel;
-  ShowShopFoodMenu({Key key, this.userModel}) :super(key: key);
+  ShowShopFoodMenu({Key key, this.userModel}) : super(key: key);
   @override
   _ShowShopFoodMenuState createState() => _ShowShopFoodMenuState();
 }
 
 class _ShowShopFoodMenuState extends State<ShowShopFoodMenu> {
   UserModel userModel;
+  List<Widget> listWidgets = List();
+  int indexPage = 0;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     userModel = widget.userModel;
+    listWidgets.add(AboutShop(
+      userModel: userModel,
+    ));
+    listWidgets.add(ShowMenuFood());
+  }
+
+  BottomNavigationBarItem aboutShopNav() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.restaurant),
+      title: Text('รายละเอียดร้าน'),
+    );
+  }
+
+  BottomNavigationBarItem showMenuFoodNav() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.restaurant_menu),
+      title: Text('เมนูอาหาร'),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(userModel.nameShop),),
+      appBar: AppBar(
+        title: Text(userModel.nameShop),
+      ),
+      body: listWidgets.length == 0 ? MyStyle().showProgress():listWidgets[indexPage],
+      bottomNavigationBar: showBottonNavigationBar(),
     );
   }
+
+  BottomNavigationBar showBottonNavigationBar() => BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.white,
+        currentIndex: indexPage,
+        onTap: (value) {
+          setState(() {
+            indexPage = value;
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          aboutShopNav(),
+          showMenuFoodNav(),
+        ],
+      );
 }
