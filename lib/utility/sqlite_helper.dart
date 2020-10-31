ca95a57b9b97a7c7977656e6c064f836964f9895
systemFood <cukjsystem@gmail.com>
2020-10-31 16:49:27 +0700

    update SQflite 31-10-63

---
+++ lib/utility/sqlite_helper.dart
@@ -0,0 +1,11 @@
+class SQLiteHelper {
+  final String nameDatabase = 'riwFood.db';
+  final String tableDatabase = 'orderTABLE';
+  int version = 1;
+
+  SQLiteHelper() {
+    initDatabase();
+  }
+
+  initDatabase() {}
+}
