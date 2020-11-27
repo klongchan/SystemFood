efbb2036fbb4c14d6d21942e8c89b05372692bab
systemFood <cukjsystem@gmail.com>
2020-11-27 16:27:38 +0700

    update-listorder27-11-63

--- lib/widget/show_status_food_order.dart
+++ lib/widget/show_status_food_order.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:systemfood/model/order_model.dart';
+import 'package:systemfood/utility/my_constant.dart';
 
 class ShowStatusFoodOrder extends StatefulWidget {
   @override
@@ -7,11 +12,53 @@ class ShowStatusFoodOrder extends StatefulWidget {
 }
 
 class _ShowStatusFoodOrderState extends State<ShowStatusFoodOrder> {
+  String idUser;
+  bool statusOrder = true;
+  List<OrderModel> orderModels = List();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    findUser();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'This is Show Status Food Order'
-      
-    );
+    return statusOrder ? buildNonOrder() : buildContent();
+  }
+
+  Widget buildContent() => ListView.builder(
+        itemCount: orderModels.length,
+        itemBuilder: (context, index) => Text(orderModels[index].nameFood),
+      );
+
+  Center buildNonOrder() =>
+      Center(child: Text('ยังไม่เคยมี ข้อมูลการสั่งอาหาร'));
+
+  Future<Null> findUser() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    idUser = preferences.getString('id');
+    print('idUser = $idUser');
+    readOrderFromIdUser();
+  }
+
+  Future<Null> readOrderFromIdUser() async {
+    if (idUser != null) {
+      String url =
+          '${MyConstant().domain}/RiwFood/getOrderWhereIdUser.php?isAdd=true&idUser=$idUser';
+
+      Response response = await Dio().get(url);
+      print('respose ==> $response');
+      if (response.toString() != 'null') {
+        var result = jsonDecode(response.data);
+        for (var map in result) {
+          OrderModel model = OrderModel.fromJson(map);
+          setState(() {
+            statusOrder = false;
+            orderModels.add(model);
+          });
+        }
+      }
+    }
   }
 }
