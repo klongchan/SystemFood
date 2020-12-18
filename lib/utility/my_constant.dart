36c7dd1c7e93ed7db4aae83f5306426ab5ea1a23
systemFood <cukjsystem@gmail.com>
2020-12-18 15:12:41 +0700

    updatereadOrder18-12-63

--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -1,6 +1,9 @@
 class MyConstant {
   // String domain = 'http://192.168.56.1';
-  String domain = 'http://8cef47b04a00.ngrok.io';
+  String domain = 'http://7a6bbb652c80.ngrok.io';
+  String keyId = 'id';
+  String keyType = 'ChooseType';
+  String keyName = 'Name';
 
   MyConstant();
 }
